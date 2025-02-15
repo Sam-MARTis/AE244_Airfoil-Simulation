ca941ea5fd684e66d1a5f60e06a510bb0a208e15
Sam-MARTis <samanthmartis@gmail.com>
2025-02-15 13:23:45 +0530

    Added chord plotting and menu

--- main.js
+++ main.js
@@ -3,12 +3,18 @@ const canvas = document.getElementById("projectCanvas");
 canvas.width = window.innerWidth * devicePixelRatio;
 canvas.height = window.innerHeight * devicePixelRatio;
 const ctx = canvas.getContext("2d");
+const width = canvas.width;
+const height = canvas.height;
 // First better take input of airofil parameters
 let airfoilType = 1; // 0 if custom, 1 if NACA 4 digit, 2 if NACA 5 digit
-let M = 0.02;
-let P = 0.4;
-let span = 4;
+let M = 0.04;
+let P = 0.3;
+let chordLength = 4;
 let T = 0.12;
+const MMenu = document.getElementById('M');
+const PMenu = document.getElementById('P');
+const ChordLengthMenu = document.getElementById('ChordLength');
+const submitBut = document.getElementById("Submit");
 let camberFunction = (x) => {
     return 0;
 };
@@ -21,12 +27,15 @@ const initializeCamberFunction = () => {
     }
     else if (airfoilType == 1) {
         camberFunction = (x) => {
-            const xc = x / span;
-            if (x < P) {
-                return (M / Math.pow(P, 2)) * (2 * P * xc - Math.pow(xc, 2));
+            const xc = x / chordLength;
+            if (xc < P) {
+                return chordLength * (M / Math.pow(P, 2)) * (2 * P * xc - Math.pow(xc, 2));
+            }
+            else if (xc > P) {
+                return (chordLength * (M / Math.pow(1 - P, 2)) * (1 - 2 * P + 2 * P * xc - Math.pow(xc, 2)));
             }
             else {
-                return ((M / Math.pow(1 - P, 2)) * (1 - 2 * P + 2 * P * xc - Math.pow(xc, 2)));
+                return -100;
             }
         };
     }
@@ -38,15 +47,44 @@ const initializeCamberFunction = () => {
     }
 };
 const plotCamberLine = (xStart, yStart, pointCount, scaleFactor, lwidth, colour) => {
-    const dx = span / (pointCount - 1);
+    const dx = chordLength / (pointCount - 1);
     ctx.beginPath();
     ctx.lineWidth = lwidth;
     ctx.strokeStyle = colour;
     ctx.moveTo(xStart, yStart);
     for (let i = 0; i < pointCount; i++) {
+        // ctx.beginPath()
         const x = i * dx;
         const y = camberFunction(x);
-        ctx.lineTo(xStart + x * scaleFactor, yStart + y * scaleFactor);
+        ctx.lineTo(xStart + x * scaleFactor, yStart - y * scaleFactor);
+        ctx.moveTo(xStart + x * scaleFactor, yStart - y * scaleFactor);
+        ctx.stroke();
     }
     ctx.stroke();
 };
+const getUserMenuInput = () => {
+    M = parseFloat(MMenu.value) / 100;
+    P = parseFloat(PMenu.value) / 10;
+    chordLength = parseFloat(ChordLengthMenu.value);
+};
+// initializeCamberFunction()
+// plotCamberLine(100, 100, 500, 100, 1, "red")
+let xOffset = width / 2;
+let yOffset = height / 2;
+const DEFAULT_LINE_THICKNESS = 1;
+const CAMBER_COLOUR = "red";
+const DRAW_SCALE_FACTOR = 100;
+const performPlotOperation = (pointCount) => {
+    getUserMenuInput();
+    xOffset = canvas.width / 2 - chordLength * DRAW_SCALE_FACTOR / 2;
+    yOffset = canvas.height / 2 + M * chordLength * DRAW_SCALE_FACTOR / 2;
+    initializeCamberFunction();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    plotCamberLine(xOffset, yOffset, pointCount, DRAW_SCALE_FACTOR, DEFAULT_LINE_THICKNESS, CAMBER_COLOUR);
+};
+performPlotOperation(200);
+submitBut.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("Submitted");
+    performPlotOperation(200);
+});
--- main.ts
+++ main.ts
@@ -2,14 +2,24 @@ const canvas = document.getElementById("projectCanvas") as HTMLCanvasElement;
 canvas.width = window.innerWidth * devicePixelRatio;
 canvas.height = window.innerHeight * devicePixelRatio;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+const width = canvas.width
+const height = canvas.height
 
 // First better take input of airofil parameters
 let airfoilType = 1; // 0 if custom, 1 if NACA 4 digit, 2 if NACA 5 digit
-let M = 0.02;
-let P = 0.4;
-let span = 4;
+let M = 0.04;
+let P = 0.3;
+let chordLength = 4;
 
 let T = 0.12;
+
+
+
+const MMenu = document.getElementById('M') as HTMLInputElement
+const PMenu = document.getElementById('P') as HTMLInputElement
+const ChordLengthMenu = document.getElementById('ChordLength') as HTMLInputElement
+const submitBut = document.getElementById("Submit") as HTMLElement;
+
 
 let camberFunction = (x: number): number => {
   return 0;
@@ -25,13 +35,16 @@ const initializeCamberFunction = () => {
 
 
     camberFunction = (x: number): number => {
-      const xc = x / span;
-      if (x < P) {
-        return (M / Math.pow(P, 2)) * (2 * P * xc - Math.pow(xc, 2));
-      } else {
+      const xc = x / chordLength;
+      if (xc < P) {
+        return chordLength*(M / Math.pow(P, 2)) * (2 * P * xc - Math.pow(xc, 2));
+      } else if(xc>P) {
         return (
-          (M / Math.pow(1 - P, 2)) * (1 - 2 * P + 2 * P * xc - Math.pow(xc, 2))
+          chordLength*(M / Math.pow(1 - P, 2)) * (1 - 2 * P + 2 * P * xc - Math.pow(xc, 2))
         );
+      }
+      else{
+        return -100
       }
     };
 
@@ -46,18 +59,52 @@ const initializeCamberFunction = () => {
 };
 
 const plotCamberLine = (xStart:number, yStart: number, pointCount: number,  scaleFactor: number, lwidth: number, colour:string) => {
-    const dx = span/(pointCount-1);
+    const dx = chordLength/(pointCount-1);
     ctx.beginPath()
     ctx.lineWidth = lwidth
     ctx.strokeStyle = colour
     ctx.moveTo(xStart, yStart)
 
     for(let i = 0; i<pointCount; i++){
+        // ctx.beginPath()
         const x = i*dx
         const y = camberFunction(x)
-        ctx.lineTo(xStart+x*scaleFactor, yStart+y*scaleFactor)
+        ctx.lineTo(xStart+x*scaleFactor, yStart-y*scaleFactor)
+        ctx.moveTo(xStart+x*scaleFactor, yStart-y*scaleFactor)
+        ctx.stroke()
     }
     ctx.stroke()
 }
 
+
+const getUserMenuInput = () => {
+    M = parseFloat(MMenu.value)/100;
+    P = parseFloat(PMenu.value)/10;
+    chordLength = parseFloat(ChordLengthMenu.value); 
+
+}
+// initializeCamberFunction()
+// plotCamberLine(100, 100, 500, 100, 1, "red")
+let xOffset = width/2
+let yOffset = height/2
+const DEFAULT_LINE_THICKNESS = 1
+const CAMBER_COLOUR = "red"
+const DRAW_SCALE_FACTOR = 100
+
+const performPlotOperation = (pointCount: number) => {
+    getUserMenuInput()
+    xOffset = canvas.width/2 - chordLength*DRAW_SCALE_FACTOR/2
+    yOffset = canvas.height/2 + M*chordLength*DRAW_SCALE_FACTOR/2
+    initializeCamberFunction()
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    plotCamberLine(xOffset, yOffset, pointCount, DRAW_SCALE_FACTOR, DEFAULT_LINE_THICKNESS, CAMBER_COLOUR);
+}
+
+performPlotOperation(200)
+
+submitBut.addEventListener("click", (e: Event) => {
+    e.preventDefault();
+    console.log("Submitted");
+    performPlotOperation(200);
+  });
 
