8eb072f3079d54c764224b82d2e1b9f780c135e1
Sam-MARTis <samanthmartis@gmail.com>
2025-03-09 14:27:22 +0530

    Added calculation tasks and ui update

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let chordLength = 1;
 const pointCount = 500;
 const defaultIntegrationAccuracy = 0.0001;
 let T = 0.12;
-const Uinfty = 1;
+const Uinfty = 20;
 let AOA = (0 * Math.PI) / 180;
 let AnCache = [];
 let airfoilCirculationCache = [];
@@ -29,8 +29,10 @@ const ChordLengthMenu = document.getElementById("ChordLength");
 const submitBut = document.getElementById("Submit");
 const plotOptionMenu = document.getElementById("plotOptions");
 const AOAMenu = document.getElementById("AOA");
-const circulationSubmitBut = document.getElementById("circulationSubmit");
+// const circulationSubmitBut = document.getElementById("circulationSubmit") as HTMLElement;
 const circulationOutput = document.getElementById("circulationOutput");
+// const CLSubmitBut = document.getElementById("CLSubmit") as HTMLElement;
+const CLOutput = document.getElementById("CLOutput");
 let camberFunction = (x) => {
     return 0;
 };
@@ -181,23 +183,71 @@ const mapCanvasToSpace = (canvasX, canvasY, shouldFloor = false) => {
     }
     return [spaceX, spaceY];
 };
-const plotAirfoilFunction = (functionIn, pointCount, lwidth, colour) => {
+const plotAirfoilFunction = (functionIn, pointCount, lwidth, colour, title = "Camber Slope") => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     const dx = chordLength / (pointCount - 1);
     ctx.beginPath();
     ctx.lineWidth = lwidth;
     ctx.strokeStyle = colour;
     ctx.moveTo(...mapSpaceToCanvas(0, functionIn(0)));
+    const xVals = [];
+    const yVals = [];
     for (let i = 0; i < pointCount; i++) {
         // ctx.beginPath()
         const x = i * dx;
         const y = functionIn(x);
+        xVals.push(x);
+        yVals.push(y);
         const dr = mapSpaceToCanvas(x, y);
         ctx.lineTo(dr[0], dr[1]);
         ctx.moveTo(dr[0], dr[1]);
         ctx.stroke();
     }
     ctx.stroke();
+    const minX = Math.min(...xVals);
+    const maxX = Math.max(...xVals);
+    const minY = Math.min(...yVals);
+    const maxY = Math.max(...yVals);
+    ctx.beginPath();
+    ctx.font = "20px Arial";
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 1;
+    ctx.moveTo(...mapSpaceToCanvas(minX, minY));
+    ctx.textAlign = "right";
+    ctx.strokeText(`(${minX.toFixed(3)}, ${minY.toFixed(3)})`, ...mapSpaceToCanvas(minX, minY));
+    ctx.lineTo(...mapSpaceToCanvas(minX, maxY));
+    ctx.strokeText(`(${minX.toFixed(3)}, ${maxY.toFixed(3)})`, ...mapSpaceToCanvas(minX, maxY));
+    ctx.moveTo(...mapSpaceToCanvas(minX, minY));
+    ctx.lineTo(...mapSpaceToCanvas(maxX, minY));
+    ctx.textAlign = "left";
+    ctx.strokeText(`(${maxX.toFixed(3)}, ${minY.toFixed(3)})`, ...mapSpaceToCanvas(maxX, minY));
+    // ctx.moveTo(...mapSpaceToCanvas((maxX+minX)/2, minY*1.5))
+    ctx.textAlign = "center";
+    const topY = mapCanvasToSpace(0, canvas.height / 4)[1];
+    ctx.strokeText(title, ...mapSpaceToCanvas((maxX + minX) / 2, topY * 1));
+    ctx.stroke();
+};
+const plotAirfoilFunctionImage = (functionIn, pointCount, lwidth, colour) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const dx = chordLength / (pointCount - 1);
+    // ctx.beginPath();
+    // ctx.lineWidth = lwidth;
+    // ctx.strokeStyle = colour;
+    // ctx.moveTo(...mapSpaceToCanvas(0, functionIn(0)));
+    const xVals = [];
+    const yVals = [];
+    for (let i = 0; i < pointCount; i++) {
+        // ctx.beginPath()
+        const x = i * dx;
+        const y = functionIn(x);
+        xVals.push(x);
+        yVals.push(y);
+        // const dr = mapSpaceToCanvas(x, y);
+        // ctx.lineTo(dr[0], dr[1]);
+        // ctx.moveTo(dr[0], dr[1]);
+        // ctx.stroke();
+    }
+    // ctx.stroke();
 };
 const plotCamberSlope = (pointCount, lwidth, colour) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -234,19 +284,20 @@ const performPlotOperation = (pointCount) => {
     switch (thingToPlot) {
         case "camberLine":
             initializeCamberFunction();
-            plotAirfoilFunction(camberFunction, pointCount, DEFAULT_LINE_THICKNESS, CAMBER_COLOUR);
+            plotAirfoilFunction(camberFunction, pointCount, DEFAULT_LINE_THICKNESS, CAMBER_COLOUR, "Camber Line");
             break;
         case "camberSlope":
-            plotAirfoilFunction(camberSlope, pointCount, DEFAULT_LINE_THICKNESS, CAMBER_COLOUR);
+            plotAirfoilFunction(camberSlope, pointCount, DEFAULT_LINE_THICKNESS, CAMBER_COLOUR, "Camber Slope");
             break;
     }
 };
 //Circulation calculation function
 const calculateAndPlotCirculationViaLineIntegral = (dx = 0.01) => {
     let circValue = 0;
-    const SWCornerCoords = [-chordLength * 0.1, -chordLength * M * 3];
+    const padding = 0.1;
+    const SWCornerCoords = [-chordLength * 0.1 - padding, -chordLength * M * 3 - padding];
     // const NECornerCoords = mapCanvasToSpace(canvas.width*(1/4), canvas.height*(1/5), true)
-    const NECornerCoords = [chordLength * 1.1, chordLength * M * 3];
+    const NECornerCoords = [chordLength * 1.1 + padding, chordLength * M * 3 + padding];
     for (let i = SWCornerCoords[0]; i < NECornerCoords[0]; i += dx) {
         circValue += getVelocityAtPoint(i, NECornerCoords[1])[0] * dx - getVelocityAtPoint(i, SWCornerCoords[1])[0] * dx;
     }
@@ -265,26 +316,27 @@ const calculateAndPlotCirculationViaLineIntegral = (dx = 0.01) => {
 };
 // console.log("Circulation value is: ", circValue)
 // Vector field plotting
-const plotVectorField = (spacing = 0.1) => {
+const plotVectorField = (spacing = 0.2) => {
     const SWCornerCoords = mapCanvasToSpace(0, canvas.height, false);
     const NECornerCoords = mapCanvasToSpace(canvas.width, 0, false);
     ctx.strokeStyle = "blue";
     ctx.lineWidth = 2;
     const dx = 20;
+    const normalizingFactor = 2;
+    const sensitivity = 1;
+    const biasVal = 10;
     console.log('Drawing vector fields');
     console.log(SWCornerCoords, NECornerCoords);
     for (let i = SWCornerCoords[0]; i < NECornerCoords[0]; i += spacing) {
         for (let j = SWCornerCoords[1]; j < NECornerCoords[1]; j += spacing) {
             const vel = getVelocityAtPoint(i, j);
-            const sensitivity = 1;
-            const biasVal = 10;
-            const plottingVal = biasVal * (255 / (6.2830)) * Math.exp(-sensitivity * ((vel[0] ** 2 + vel[1] ** 2)));
+            const plottingVal = biasVal * (255 / (6.2830)) * Math.exp(-sensitivity * (((vel[0] / (Uinfty)) ** 2 + (vel[1] / (Uinfty)) ** 2)));
             console.log(plottingVal);
             ctx.strokeStyle = `rgb(${255 - plottingVal}, 0, ${plottingVal})`;
             const r = mapSpaceToCanvas(i, j);
             ctx.beginPath();
             ctx.moveTo(r[0], r[1]);
-            ctx.lineTo(r[0] + vel[0] * dx, r[1] - vel[1] * dx);
+            ctx.lineTo(r[0] + (vel[0] / (Uinfty * normalizingFactor)) * dx, r[1] - (vel[1] / (Uinfty * normalizingFactor)) * dx);
             // console.log("Moving to ", r)
             ctx.stroke();
         }
@@ -297,8 +349,41 @@ const setup = (n = 20) => {
     initializeCirculationFunction();
     cacheAirfoilCirculation();
     plotVectorField();
+    // circulationSubmitButTask()
+    calculationTasks();
 };
-setup();
+//Event functions
+const plotOptionMenuTask = () => {
+    thingToPlot = plotOptionMenu.value;
+    performPlotOperation(pointCount);
+    if (thingToPlot == "camberLine") {
+        // setup()
+        plotVectorField();
+    }
+};
+const circulationSubmitButTask = () => {
+    const circVal = calculateAndPlotCirculationViaLineIntegral(0.001);
+    circulationOutput.innerHTML = `Circulation value is: ${circVal}  &nbsp;  <br>Bound circulation value is: ${plotBoundCirculationOfCamber()}`;
+};
+const CLSubmitButTask = () => {
+    const circVal = calculateAndPlotCirculationViaLineIntegral(0.001);
+    const CLViaLift = circVal / (0.5 * (Uinfty ** 1) * chordLength);
+    if (AnCache.length < 2) {
+        cacheAn(15);
+    }
+    const CLViaAn = Math.PI * (2 * AnCache[0] + AnCache[1]);
+    CLOutput.innerHTML = `Lift Coefficient via Kutta Joukowski: ${CLViaLift} &nbsp; <br> Lift Coefficient via An is: ${CLViaAn}`;
+};
+const calculationTasks = () => {
+    const circVal = calculateAndPlotCirculationViaLineIntegral(0.001);
+    circulationOutput.innerHTML = `Circulation value is: ${circVal}  &nbsp;  <br>Bound circulation value is: ${plotBoundCirculationOfCamber()}`;
+    const CLViaLift = circVal / (0.5 * (Uinfty ** 1) * chordLength);
+    if (AnCache.length < 2) {
+        cacheAn(15);
+    }
+    const CLViaAn = Math.PI * (2 * AnCache[0] + AnCache[1]);
+    CLOutput.innerHTML = `Lift Coefficient via Kutta Joukowski: ${CLViaLift} &nbsp; <br> Lift Coefficient via pi(2A0 + A1) is: ${CLViaAn}`;
+};
 //Event listeners
 submitBut.addEventListener("click", (e) => {
     e.preventDefault();
@@ -306,18 +391,18 @@ submitBut.addEventListener("click", (e) => {
     performPlotOperation(pointCount);
     console.log(getAn(0));
     setup();
-    plotVectorField();
+    //   circulationSubmitButTask()
+    // CLSubmitButTask()
+    // plotVectorField()
     // cacheAn(15)
     // console.log(AnCache)
 });
 plotOptionMenu.addEventListener("change", () => {
-    // Update the PlotOption variable to the selected value
-    thingToPlot = plotOptionMenu.value;
-    performPlotOperation(pointCount);
+    plotOptionMenuTask();
 });
 document.addEventListener("click", (e) => {
     cacheAn(15);
-    // console.log(AnCache);
+    console.log(AnCache[0]);
     // console.log(airfoilCirculationCache);
     console.log(getVelocityAtPoint(...mapSpaceToCanvas(e.layerX, e.layerY)));
 });
@@ -330,10 +415,12 @@ document.addEventListener("mousemove", (e) => {
     // ctx.lineTo(...mapSpaceToCanvas(i + 2*vx, j+2*vy))
     // ctx.stroke()
 });
-circulationSubmitBut.addEventListener("click", (e) => {
-    e.preventDefault();
-    const circVal = calculateAndPlotCirculationViaLineIntegral(0.001);
-    circulationOutput.innerHTML = `Circulation value is: ${circVal}  &nbsp;  <br>Bound circulation value is: ${plotBoundCirculationOfCamber()}`;
-    // setup()
-    // plotVectorField()
-});
+// circulationSubmitBut.addEventListener("click", (e:Event) => {
+//   e.preventDefault()
+//   circulationSubmitButTask()
+// })
+// CLSubmitBut.addEventListener("click", (e:Event) => {
+//   e.preventDefault()
+//   CLSubmitButTask()
+// })
+setup();
